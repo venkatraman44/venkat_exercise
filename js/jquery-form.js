b21137aaf215c36b3b4b55dc63b4a08a37bb6338
venkatraman <kmlvenkat@gmail.com>
2023-06-28 17:44:17 +0530

    fshfh

--- js/jquery-form.js
+++ js/jquery-form.js
@@ -1,12 +1,19 @@
-(function ($, Drupal) {
+(function ($, Drupal, drupalSettings) {
+
+  Drupal.behaviors.MyModuleBehavior = {
+      attach: function (context, settings) {
+      var testing = drupalSettings.venkat_exercise.testing;
+      console.log(testing)
+      $('body').css('background', testing);
+      }
+  };
+
   $.fn.testing = function() {
-      alert("submitted");
+  console.log("submitted");
       $("#custom-form-get-user-details").submit();
   };
 
-}(jQuery, Drupal));
 
-(function ($) {
   $(document).ready(function () {
       var $permanentAdd = $('#same-as-permanent');
       var $tempAdd = $('.form-item-temporary-address');
@@ -23,15 +30,6 @@
       }
       });
   });
-})(jQuery);
 
-(function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.MyModuleBehavior = {
-      attach: function (context, settings) {
-      var testing = drupalSettings.venkat_exercise.testing;
-      console.log(testing)
-      $('body').css('background', testing);
-      }
-  };
-  // Drupal.behaviors.MyModuleBehavior.attach(document, Drupal.settings);
-})(jQuery, Drupal, drupalSettings);
+
+}(jQuery, Drupal, drupalSettings));
